a6b39d6c993e9c03116642147b35201c27904dc3
Mnemba Chambuya <karimnembac@gmail.com>
2022-02-07 17:58:14 +0300

    add local storage

--- js/dynamics.js
+++ js/dynamics.js
@@ -57,5 +57,50 @@ bookForm.addEventListener('submit', (e) => {
 
 addBookButton.addEventListener('click', (e) => {
   addBook(bookTitle.value, bookAuthor.value);
-  console.log(books);
+  populateStorage()
 });
+
+
+// Local storage
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+    // everything except Firefox
+      e.code === 22
+            // Firefox
+            || e.code === 1014
+            // test name field too, because code might not be present
+            // everything except Firefox
+            || e.name === 'QuotaExceededError'
+            // Firefox
+            || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+            // acknowledge QuotaExceededError only if there's something already stored
+            && (storage && storage.length !== 0);
+  }
+}
+
+function setInputs() {
+  books = JSON.parse(localStorage.getItem('books')); 
+}
+
+function populateStorage() {
+
+  localStorage.setItem('books', JSON.stringify(books));
+  setInputs();
+}
+
+if (storageAvailable('localStorage')) {
+  // Yippee! We can use localStorage awesomeness
+  if (!localStorage.getItem('books')) {
+    populateStorage();
+  } else {
+    setInputs();
+  }
+}
