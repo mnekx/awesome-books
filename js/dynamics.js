ee6aa846691227413c2e476e5dc7c6f5c898374b
Mnemba Chambuya <karimnembac@gmail.com>
2022-02-07 16:10:55 +0300

    add ids from js objects

--- js/dynamics.js
+++ js/dynamics.js
@@ -1,57 +1,61 @@
+class Book {
+   static id = -1;
 
-class Book{
-    constructor(title,author) {
-        this.title = title;
-        this.tail = author;
-      }
+   constructor(title, author) {
+     this.title = title;
+     this.author = author;
+     Book.id += 1;
+     this.idx = Book.id;
+   }
 }
 
-let books=[{title:'harry putter',author:'alvin bbb'},{title:'pkdqslkdmlqs',author:'alvin bbb'}];
+let books = [];
 
-function addBook(title,author){
-   const book = new Book(title,author);
-   books.push(book);
+function addBook(title, author) {
+  const book = new Book(title, author);
+  books.push(book);
 }
 
-function remove(index){
-   books = books.filter(item,idx=>idx!==index);
+function remove(index) {
+  books = books.filter(item, (idx) => idx !== index);
 }
 
 /* events */
 
 const booksContainer = document.querySelector('.books');
 const addBookButton = document.querySelector('#add-book');
-const bookTitle= document.querySelector('#title');
-const bookAuthor= document.querySelector('#author');
-const bookForm=document.querySelector('form');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookForm = document.querySelector('form');
 
-function displayBooks(bookList){
-    booksContainer.innerHTML='';
-    bookList.map(book=>{
-        booksContainer.innerHTML+= `<li>
-                                    <h2>`+book.title+`</h2>
-                                    <h2>`+book.author+`</h2>
-                                    <button>Remove</button>
+function displayBooks(bookList) {
+  booksContainer.innerHTML = '';
+  bookList.forEach((book) => {
+    booksContainer.innerHTML += `<li>
+                                    <h2>${book.title}</h2>
+                                    <h2>${book.author}</h2>
+                                    <button onclick=remove(${book.idx})>Remove</button>
                                     <hr />
-                                    </li>`
-                                });
+                                    </li>`;
+  });
 }
 if (document.readyState !== 'loading') {
-    books.map(book=>{
-        displayBooks(books)});
-  } else {
-document.addEventListener('DOMContentLoaded', () => {
+  
     displayBooks(books);
- });
- }   
+    
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    displayBooks(books);
+  });
+}
 
-bookForm.addEventListener('submit', function(e){
-   e.preventDefault();
-   console.log('after submit',books);
-   displayBooks(books);
-}) 
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('after submit', books);
+  displayBooks(books);
+});
 
-addBookButton.addEventListener('click',function(e){
-   addBook(bookTitle.value,bookAuthor.value);
-   console.log(books);
+addBookButton.addEventListener('click', (e) => {
+  addBook(bookTitle.value, bookAuthor.value);
+  console.log(books);
 });
