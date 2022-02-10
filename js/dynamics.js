3cf0017c8a6dd00589b820cf381d26cc61d125b6
Mnemba Chambuya <karimnembac@gmail.com>
2022-02-10 15:53:47 +0300

    fix linter errors

--- js/dynamics.js
+++ js/dynamics.js
@@ -1,51 +1,53 @@
 let navLinks = document.querySelectorAll('.nav-link');
 let contentSections = document.querySelectorAll('.content');
 navLinks = Array.from(navLinks);
-contentSections = Array.from(contentSections)
+contentSections = Array.from(contentSections);
 navLinks.forEach((link, key) => {
-    contentSections.forEach(section => {
-        section.classList.remove('content-show')
-        // section.classList.add('content-hide')
-    })
-    switch(key) {
-        case 0: // List link selected
-            link.addEventListener('click', (e) =>{
-                e.preventDefault();
-                let selectedSection = document.querySelector('#main-section');
-                contentSections.forEach(section => {
-                    section.classList.remove('content-show')
-                    section.classList.add('content-hide')
-                })
-                selectedSection.classList.add('content-show');
-                selectedSection.classList.remove('content-hide');
-                console.log(selectedSection);
-            })
-            break;
-        case 1: // List link selected
-            link.addEventListener('click', (e) =>{
-                e.preventDefault();
-                let selectedSection = document.querySelector('#new-book-section');
-                contentSections.forEach(section => {
-                    section.classList.remove('content-show')
-                    section.classList.add('content-hide')
-                })
-                selectedSection.classList.add('content-show');
-                selectedSection.classList.remove('content-hide');
-                console.log(selectedSection);
-            })
-            break;
-        case 2: // List link selected
-            link.addEventListener('click', (e) =>{
-                e.preventDefault();
-                let selectedSection = document.querySelector('#contact-section');
-                contentSections.forEach(section => {
-                    section.classList.remove('content-show')
-                    section.classList.add('content-hide')
-                })
-                selectedSection.classList.add('content-show');
-                selectedSection.classList.remove('content-hide');
-                console.log(selectedSection);
-            })
-            break;
-    }
-})
+  contentSections.forEach((section) => {
+    section.classList.remove('content-show');
+    // section.classList.add('content-hide')
+  });
+  switch (key) {
+    case 0: // List link selected
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedSection = document.querySelector('#main-section');
+        contentSections.forEach((section) => {
+          section.classList.remove('content-show');
+          section.classList.add('content-hide');
+        });
+        selectedSection.classList.add('content-show');
+        selectedSection.classList.remove('content-hide');
+        console.log(selectedSection);
+      });
+      break;
+    case 1: // List link selected
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedSection = document.querySelector('#new-book-section');
+        contentSections.forEach((section) => {
+          section.classList.remove('content-show');
+          section.classList.add('content-hide');
+        });
+        selectedSection.classList.add('content-show');
+        selectedSection.classList.remove('content-hide');
+        console.log(selectedSection);
+      });
+      break;
+    case 2: // List link selected
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedSection = document.querySelector('#contact-section');
+        contentSections.forEach((section) => {
+          section.classList.remove('content-show');
+          section.classList.add('content-hide');
+        });
+        selectedSection.classList.add('content-show');
+        selectedSection.classList.remove('content-hide');
+        console.log(selectedSection);
+      });
+      break;
+    default:
+      break;
+  }
+});
